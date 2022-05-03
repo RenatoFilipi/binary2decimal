3601d8bb99fc9d195f83a4587b236e36a4cf497f
Renato Filipi <re.rosa98@gmail.com>
2022-05-03 08:59:27 -0300

    clear default

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        qs: "'Quicksand', sans-serif",
+      },
+    },
+  },
+  plugins: [],
+};
