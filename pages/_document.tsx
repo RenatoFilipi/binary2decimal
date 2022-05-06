36306a92b9451d10bd3d7bf7e226467582b55144
Renato Filipi <re.rosa98@gmail.com>
2022-05-06 10:13:12 -0300

    pwa and lighthouse

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,8 +2,12 @@ import { Head, Html, Main, NextScript } from "next/document";
 
 const Document = () => {
   return (
-    <Html>
+    <Html lang="en">
       <Head>
+        <meta
+          name="description"
+          content="This is a Binary to Decimal converter website."
+        />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
